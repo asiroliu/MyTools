1541da57da394dc92cafd422b2c5dad433325b88
asiroliu <asiro@qq.com>
2015-03-24 10:31:15 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+"""
+    Name:       __init__.py
+    Author:     Andy Liu
+    Email :     anx.liu@intel.com
+    Created:    3/24/2015
+    Copyright:  © intel 2015
+    Licence:    This program is free software: you can redistribute it 
+    and/or modify it under the terms of the GNU General Public License 
+    as published by the Free Software Foundation, either version 3 of 
+    the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
